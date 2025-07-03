b3d0fe88ab13f48fa4c56070df365839471f8b58
Caio Beckmann <caio@onfireprodutora.com>
2025-07-03 00:07:53 -0300

    Separar canais: diário e comandos

--- index.js
+++ index.js
@@ -15,15 +15,17 @@ client.once('ready', () => {
   console.log(`🤖 Bot online como ${client.user.tag}`);
 
   // Agendamento diário
-  cron.schedule('0 10 * * *', async () => {
-    const channel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
-    const message = await fetchLadderRanking();
-    channel.send(message);
-  });
+ cron.schedule('0 10 * * *', async () => {
+  const channel = await client.channels.fetch(process.env.DAILY_RANKING_CHANNEL_ID);
+  const message = await fetchLadderRanking();
+  channel.send(message);
+});
 });
 
 // Comando manual !ranking
 client.on('messageCreate', async (message) => {
+  if (message.channel.id !== process.env.COMMANDS_CHANNEL_ID) return;
+
   if (message.content === '!ranking' && !message.author.bot) {
     const loadingMessage = await message.channel.send('🔄 Buscando ranking da Ladder...');
     const ranking = await fetchLadderRanking();
